69cd5cb63528a02f08e5e1f399bbc24a2fadb372
Tonos Robert Edward (Robbie) <109938015+robbietonos37@users.noreply.github.com>
2023-03-21 10:10:18 -0500

    Add more Notes in JS

--- notes-app.js
+++ notes-app.js
@@ -1,3 +1,14 @@
+const notes = [{
+    title: 'my next Trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habits to work on',
+    body: 'Exercise. Eating a bit better.'
+}, {
+    title: 'Office modifications',
+    body: 'Get a new seat'
+}]
+
 //DOM - Document Object model
 
 //query and remove
@@ -12,3 +23,8 @@ ps.forEach(function (p) {
     //console.log(p.textContent)
     //p.remove()
 })
+
+//Add new element
+const newParagraph = document.createElement('p')
+newParagraph.textContent = 'This is a new element from JavaScipt'
+document.querySelector('body').appendChild(newParagraph)
