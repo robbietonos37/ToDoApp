00b7bd6472a6fff9c65fabef26e5d92efe89db66
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2023-03-25 20:44:13 -0500

    Remove delete note button and create name form

--- notes-app.js
+++ notes-app.js
@@ -9,16 +9,37 @@ const notes = [{
     body: 'Get a new seat'
 }]
 
+const filters = {
+    searchText: ''
+}
+
+const renderNotes = function (notes, filters) {
+    const filteredNotes = notes.filter(function (note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    document.querySelector('#notes').innerHTML = ''
+
+    filteredNotes.forEach(function (note) {
+        const noteEl = document.createElement('p')
+        noteEl.textContent = note.title
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
+
+renderNotes(notes, filters)
+
 document.querySelector('#create-note').addEventListener('click', function (event) {
     event.target.textContent = 'The Button was clicked'
 })
 
-document.querySelector('#remove-all').addEventListener('click', function () {
-    document.querySelectorAll('.note').forEach(function (note) {
-        note.remove()
-    })
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
 })
 
-document.querySelector('#search-text').addEventListener('change', function (e) {
-    console.log(e.target.value)
+document.querySelector('#name-form').addEventListener('submit', function (e) {
+    e.preventDefault()
+    console.log(e.target.elements.firstName.value)
+    e.target.elements.firstName.value = ''
 })
