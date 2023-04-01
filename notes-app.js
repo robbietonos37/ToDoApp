8408a006324c57d7f7661a1237d72270a3aff04a
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2023-04-01 14:20:14 -0500

    Refactor functions into new js file

--- notes-app.js
+++ notes-app.js
@@ -4,30 +4,11 @@ const filters = {
     searchText: ''
 }
 
-const renderNotes = function (notes, filters) {
-    const filteredNotes = notes.filter(function (note) {
-        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
-    })
-
-    document.querySelector('#notes').innerHTML = ''
-
-    filteredNotes.forEach(function (note) {
-        const noteEl = document.createElement('p')
-        if (note.title.length > 0) {
-            noteEl.textContent = note.title
-        }
-        else {
-            noteEl.textContent = 'Unnamed Note'
-        }
-        document.querySelector('#notes').appendChild(noteEl)
-    })
-}
-
 renderNotes(notes, filters)
 
 document.querySelector('#create-note').addEventListener('click', function (event) {
     notes.push({ title: '', body: '' })
-    localStorage.setItem('notes', JSON.stringify(notes))
+    savedNotes(notes)
     renderNotes(notes, filters)
 })
 
--- notes-functions.js
+++ notes-functions.js
@@ -10,3 +10,36 @@ const getSavedNotes = function () {
     }
 
 }
+
+// Save the notes to localStorage
+const savedNotes = function (notes) {
+    localStorage.setItem('notes', JSON.stringify(notes))
+}
+
+// Generate the DOM structure for a note
+const generateNoteDOM = function (note) {
+    const noteEl = document.createElement('p')
+    if (note.title.length > 0) {
+        noteEl.textContent = note.title
+    }
+    else {
+        noteEl.textContent = 'Unnamed Note'
+    }
+
+    return noteEl
+}
+
+// Render application notes
+
+const renderNotes = function (notes, filters) {
+    const filteredNotes = notes.filter(function (note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    document.querySelector('#notes').innerHTML = ''
+
+    filteredNotes.forEach(function (note) {
+        const noteEl = generateNoteDOM(note)
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
