13be73f939edd97fafaf45f1c2fc75794f0f0629
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2023-04-01 00:08:52 -0500

    Test adding data to local storage and our array

--- notes-app.js
+++ notes-app.js
@@ -1,34 +1,17 @@
-const notes = [{
-    title: 'my next Trip',
-    body: 'I would like to go to Spain'
-}, {
-    title: 'Habits to work on',
-    body: 'Exercise. Eating a bit better.'
-}, {
-    title: 'Office modifications',
-    body: 'Get a new seat'
-}]
+let notes = []
 
 const filters = {
     searchText: ''
 }
 
-/* const user = {
-    name: 'Robert',
-    age: 25
-} */
+//Checking for existing saved data
 
-/* const userJSON = JSON.stringify(user)
+const notesJSON = localStorage.getItem('notes')
 
-console.log(userJSON)
+if (notesJSON !== null) {
+    notes = JSON.parse(notesJSON)
+}
 
-localStorage.setItem('user', userJSON) */
-
-const userJSON = localStorage.getItem('user')
-
-const user = JSON.parse(userJSON)
-
-console.log(`${user.name} is ${user.age}`)
 
 const renderNotes = function (notes, filters) {
     const filteredNotes = notes.filter(function (note) {
@@ -39,7 +22,12 @@ const renderNotes = function (notes, filters) {
 
     filteredNotes.forEach(function (note) {
         const noteEl = document.createElement('p')
-        noteEl.textContent = note.title
+        if (note.title.length > 0) {
+            noteEl.textContent = note.title
+        }
+        else {
+            noteEl.textContent = 'Unnamed Note'
+        }
         document.querySelector('#notes').appendChild(noteEl)
     })
 }
@@ -47,7 +35,9 @@ const renderNotes = function (notes, filters) {
 renderNotes(notes, filters)
 
 document.querySelector('#create-note').addEventListener('click', function (event) {
-    event.target.textContent = 'The Button was clicked'
+    notes.push({ title: '', body: '' })
+    localStorage.setItem('notes', JSON.stringify(notes))
+    renderNotes(notes, filters)
 })
 
 document.querySelector('#search-text').addEventListener('input', function (e) {
