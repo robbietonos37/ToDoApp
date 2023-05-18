6f8fa3568feb8c4ec4691bb0646250a3f020f30d
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2023-05-18 16:56:34 -0500

    Add ELs to window to maintain same data in dif tabs

--- notes-edit.js
+++ notes-edit.js
@@ -3,9 +3,9 @@ const bodyElement = document.querySelector('#note-body')
 const removeButton = document.querySelector('#remove-note')
 
 const noteId = location.hash.substring(1)
-const notes = getSavedNotes()
+let notes = getSavedNotes()
 
-const note = notes.find(function (note) {
+let note = notes.find(function (note) {
     return note.id === noteId
 })
 
@@ -30,4 +30,20 @@ removeButton.addEventListener('click', function (e) {
     removeNote(note.id)
     saveNotes(notes)
     location.assign('/index.html')
+})
+
+window.addEventListener('storage', function (e) {
+    if (e.key === 'notes') {
+        notes = JSON.parse(e.newValue)
+        note = notes.find(function (note) {
+            return note.id === noteId
+        })
+
+        if (note === undefined) {
+            location.assign('/index.html')
+        }
+
+        titleElement.value = note.title
+        bodyElement.value = note.body
+    }
 })
