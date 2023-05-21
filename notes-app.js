38bcdef87a6f6e00506332dcca0979fc20131496
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2023-05-21 18:45:59 -0500

    Add sorting ability for notes

--- notes-app.js
+++ notes-app.js
@@ -1,14 +1,16 @@
 let notes = getSavedNotes()
 
 const filters = {
-    searchText: ''
+    searchText: '',
+    sortBy: 'byEdited'
 }
 
 renderNotes(notes, filters)
 
 document.querySelector('#create-note').addEventListener('click', function (event) {
     const id = uuidv4()
-    notes.push({ id: id, title: '', body: '' })
+    const timeStamp = moment().valueOf()
+    notes.push({ id: id, title: '', body: '', createdAt: timeStamp, updatedAt: timeStamp })
     saveNotes(notes)
     location.assign(`/edit.html#${id}`)
 })
@@ -19,7 +21,8 @@ document.querySelector('#search-text').addEventListener('input', function (e) {
 })
 
 document.querySelector('#filter-by').addEventListener('change', function (e) {
-    console.log(e.target.value)
+    filters.sortBy = e.target.value
+    renderNotes(notes, filters)
 })
 
 window.addEventListener('storage', function (e) {
@@ -46,34 +49,3 @@ birthday.set('date', 2)
 //birthday.year(1997).month(7).date(2)
 console.log(birthday.format('MMM D, YYYY'))
 
-/*
-// Unix Epoch - January 1st 1970 00:00:00
-// Positive for after this date
-// Negative for after this date
-
-
-const now = new Date()
-const timestamp = now.getTime()
-
-
-const myDate = new Date(timestamp)
-console.log(myDate.getFullYear())
-
-// console.log(`Year: ${now.getFullYear()}`)
-// console.log(`Month: ${now.getMonth()}`)
-// console.log(`Day of month: ${now.getDate()}`)
-// console.log(`Hour: ${now.getHours()}`)
-// console.log(`Minutes: ${now.getMinutes()}`)
-// console.log(`Seconds: ${now.getSeconds()}`)
-
-const timeInPast = new Date('April 23 2011 13:32:43')
-const timeInPast2 = new Date('October 11 2017 5:21:07')
-
-if (timeInPast.getTime() > timeInPast2.getTime()) {
-    console.log(timeInPast.toString())
-}
-else {
-    console.log(timeInPast2.toString())
-}
-
-*/
